6850ec6d1b264a3eff987171334a2ed6bc9441de
NeoVic2006 <neoVic2006@gmail.com>
2020-10-01 16:18:01 -0500

    your commit description

---
+++ brain_games/games/nod_algorithm.py
@@ -0,0 +1,40 @@
+import random
+from random import randint
+import prompt
+
+
+# Function to create 2 random Ints and calculate NOD between them.
+# and <- User answer
+# ran_one <- its a result from NOD calculation between 2 random numbers
+def calculation():
+    ran_one = randint(1, 50)
+    ran_two = randint(1, 40)
+    print("Question: " + str(ran_one) + " " + str(ran_two))
+    ans = prompt.string('Your answer: ')
+    
+    while ran_one != ran_two:
+        if ran_one > ran_two:
+            ran_one -= ran_two
+        elif ran_two > ran_one:
+            ran_two -= ran_one
+    return ans, ran_one
+
+
+# Main function which comparing data from Calculation function and print result
+def nod_calculation():
+    count = 3
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
+    print("Find the greatest common divisor of given numbers.")
+
+    while count != 0:
+        ans, ran_one = calculation()
+        if int(ans) == ran_one:
+            print("Correct")
+        else:
+            print(ans + " is wrong answer. Correct answer was " + str(ran_one))
+            print("Let's try again, " + name + "!")
+        count -= 1
+    print("Congratulations, " + name + "!")
+
---
+++ brain_games/scripts/brain_nod.py
@@ -0,0 +1,9 @@
+from brain_games.games.nod_algorithm import nod_calculation
+
+
+def main():
+    nod_calculation()
+
+
+if __name__ == '__main__':
+    main()
