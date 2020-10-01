4ecead444911938e5221dbb8ad6d2955fe917d7e
NeoVic2006 <neoVic2006@gmail.com>
2020-10-01 14:21:10 -0500

    Version 0.3.0 Progression game was added

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,14 +1,44 @@
 import random
 from random import randint
+import prompt
 
 
-
-
-def progression():
-    list_of = []
+# Calculcation function which create random Ints, printing cleaned List and return: 
+# ans <- user answer, 
+# memor_elem <- element which was hidden 
+def calculations_for_prog():
+    list_of_num = []
     random_num= randint(1, 100)
     step = randint(1, 5)
+    find_element = randint(0, 9)
 
     for x in range(10):
-        list_of.append(random_num + step)
-    print(list_of)
+        random_num = random_num + step
+        list_of_num.append(random_num)
+
+    memor_elem = list_of_num[find_element]
+    list_of_num[find_element] = ".."
+    list_of_num = str(list_of_num).strip('[]')
+    print("Question: " + list_of_num.replace(',', ''))
+    ans = prompt.string('Your answer: ')
+    return ans, memor_elem
+
+
+#main function where ans and memor_elem are compared and show results
+def progression():
+    count = 3
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
+    print("What number is missing in the progression?")
+
+    while count != 0:
+        ans, memor_elem = calculations_for_prog()
+        if int(ans) == memor_elem:
+            print("Correct")
+        else:
+            print(ans + " is wrong answer. Correct answer was " + str(memor_elem))
+            print("Let's try again, " + name + "!")
+        count -= 1
+    print("Congratulations, " + name + "!")
+
