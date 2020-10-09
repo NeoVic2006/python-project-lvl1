eaa516ae7ba65a30eedf9a5e9b22ce98aba78fac
NeoVic2006 <NeoVic2006@gmail.com>
2020-10-09 13:30:50 -0500

    Cleaning after 2nd review

--- Test/test.py
+++ Test/test.py
@@ -1,2 +1,23 @@
-import sys
-print(sys.path)
+ 
+ 
+def greet1(name, *args):
+    string = ""
+    length = len(args)
+    for n in (name,) + args:
+        string = string + n
+        if args:
+            if length > 0:
+                string = string + " and "
+                length -= 1
+    print(string.join(['Hello, ', '!']))
+    
+    
+
+def greet(who, *args):
+    names = ' and '.join((who,) + args)
+    print('Hello, {}!'.format(names))
+
+
+greet('Bob')
+greet('Moe', 'Mary')
+greet1(*'ABC')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,31 +1,3 @@
-from brain_games.games import game_calculator
-from brain_games.games import game_progression
-from brain_games.games import game_parity
-from brain_games.games import NOD_algorithm
-from brain_games.games import fermat_algorithm
-from brain_games.scripts.add_functions import game_launch
-
-
-def start_calculator_game():
-    game_launch(game_calculator)
-
-
-def start_progression_game():
-    game_launch(game_progression)
-
-
-def start_NOD_algorithm():
-    game_launch(NOD_algorithm)
-
-
-def start_fermat_algorithm():
-    game_launch(fermat_algorithm)
-
-
-def start_parity_game():
-    game_launch(game_parity)
-
-
 def main():
     pass
 
