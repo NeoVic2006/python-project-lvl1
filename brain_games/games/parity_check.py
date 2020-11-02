f02f2cc66b76a21f88d61ead5e2cf808c80466e0
NeoVic2006 <NeoVic2006@gmail.com>
2020-11-02 12:50:09 -0600

    Updated after fifth review

--- brain_games/games/parity_check.py
+++ brain_games/games/parity_check.py
@@ -10,5 +10,3 @@ def get_question_answer():
     random_num = random.randint(RANDOM_NUMB_MIN, RANDOM_NUMB_MAX)
     result = 'yes' if random_num % 2 == 0 else 'no'
     return result, random_num
-
-
